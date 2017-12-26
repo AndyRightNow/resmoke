e6a88f06dcee357f697c9bf6c35639e1a2dd87a0
AndyRightNow <naruto199507@gmail.com>
2017-12-26 16:59:42 +0800

    Bump to 0.0.1-alpha.2

--- scripts/bump.js
+++ scripts/bump.js
@@ -2,7 +2,7 @@ var semver = require('semver');
 var fs = require('fs');
 var path = require('path');
 var packageJson = require('./../package.json');
-var exec = require('child_process').exec;
+var exec = require('child_process').execSync;
 var argv = process.argv.slice(2);
 
 var incIdentifier = argv[0];
@@ -13,12 +13,11 @@ packageJson.version = semver.inc(packageJson.version, incIdentifier, preId);
 fs.writeFileSync(path.resolve(__dirname, '../package.json'), JSON.stringify(packageJson, null, '    '));
 
 exec(
-    `git tag v${packageJson.version}`,
-    (err) => {
-        if (err) {
-            throw err;
-        }
+    `git tag v${packageJson.version}`
+);
 
-        console.log(`Bumped version to v${packageJson.version}`);
-    }
+console.log(`Bumped version to v${packageJson.version}`);
+
+exec(
+    `git add . && git commit -m "Bump to ${packageJson.version}" --no-verify`
 );
